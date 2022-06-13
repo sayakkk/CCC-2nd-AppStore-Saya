b9868480dcdaf14181780041295e39d09244ba37
sayalion <dltkdi173@gmail.com>
2022-06-13 10:22:12 +0900

    Initial Commit

---
+++ CCC-2nd-AppStore-Saya/CCC_2nd_AppStore_SayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCC_2nd_AppStore_SayaApp.swift
+//  CCC-2nd-AppStore-Saya
+//
+//  Created by 이사야 on 2022/06/13.
+//
+
+import SwiftUI
+
+@main
+struct CCC_2nd_AppStore_SayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCC-2nd-AppStore-Saya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCC-2nd-AppStore-Saya
+//
+//  Created by 이사야 on 2022/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
